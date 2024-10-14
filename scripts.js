8357541f6092dddb668d3ee731379acd0e727bc0
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2024-10-14 15:45:01 +0200

    balloon animation

--- scripts.js
+++ scripts.js
@@ -58,3 +58,41 @@ imagePopup.addEventListener('click', (event) => {
     closePopup();
   }
 });
+
+function random(num) {
+  return Math.floor(Math.random() * num);
+}
+
+function getRandomStyles() {
+  const r = random(255);
+  const g = random(255);
+  const b = random(255);
+  const left = Math.random() * 80 + 'vw';
+  const duration = random(5) + 5;
+
+  return `
+    background-color: rgba(${r},${g},${b},0.7);
+    box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
+    left: ${left};
+    animation: float ${duration}s ease-in infinite;
+  `;
+}
+
+function createBalloon() {
+  const balloon = document.createElement('div');
+  balloon.classList.add('balloon');
+  balloon.style.cssText = getRandomStyles();
+  document.getElementById('balloon-container').appendChild(balloon);
+
+  setTimeout(() => {
+    balloon.remove();
+  }, 10000);
+}
+
+function generateBalloons() {
+  for (let i = 0; i < 10; i++) {
+    setTimeout(createBalloon, i * 500);
+  }
+}
+
+window.onload = generateBalloons;
